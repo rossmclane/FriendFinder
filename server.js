c5921deadf829efe3eb186fa7d20e21aa47259fc
Ross McLane <ross.mclane@gmail.com>
2019-11-18 18:12:01 -0500

    Getting basic API endpoint structure down

---
+++ server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var PORT = process.env.PORT || 3000;
+var app = express();
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+app.listen(PORT, function(){
+    console.log(`Listening on Port ${PORT}`)
+})
