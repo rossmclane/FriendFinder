e97d8077045e94d00216700361406a057520f40a
Ross McLane <ross.mclane@gmail.com>
2019-11-18 22:45:01 -0500

    Adding POST JSON object from html

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,1 @@
-module.exports = [
-    {
-        hello: "goodbye"
-    }
-];
+module.exports = [];
--- server.js
+++ server.js
@@ -5,6 +5,8 @@ var app = express();
 // Sets up the Express app to handle data parsing
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(__dirname + '/public/home.css'));
+
 
 require("./app/routing/apiRoutes")(app);
 require("./app/routing/htmlRoutes")(app);
