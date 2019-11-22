4431570b6fd5e9f606f23bb3e8de2addb7f6518c
Ross McLane <ross.mclane@gmail.com>
2019-11-21 19:13:00 -0500

    Changing Memory

--- app/data/friends.js
+++ app/data/friends.js
@@ -8,5 +8,10 @@ module.exports = [
         name: "Sally",
         picture: "https://images.unsplash.com/photo-1572205837337-dce60116aaf8?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max",
         points: ["1", "3", "1", "1", "1", "1", "1", "1", "1", "1"]
+    },
+    {
+        name: "Damien",
+        picture: "",
+        points: ["1", "3", "1", "1", "1", "1", "1", "1", "1", "1"]
     }
 ];
